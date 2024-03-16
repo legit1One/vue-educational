6adf27a384adb29abb4a16f5654ce494cb49a406
Dauren <daukeke99@gmail.com>
2024-03-16 18:23:56 +0500

    added new hw

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,10 @@
 <template>
-  <router-view/>
+  <homework1/>
 </template>
 
 <script setup>
 import FirstDemo from "@/components/FirstDemo.vue";
+import Homework1 from "@/views/homeworks/homework1.vue";
 </script>
 
 <style>
