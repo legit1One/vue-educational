d55a0237b14dc956a03ce1ae1754c1e4decf5630
Dauren <daukeke99@gmail.com>
2024-03-20 19:45:36 +0500

    computed свойства

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,14 @@
 <template>
+<!--  {{number}}-->
+<!--  {{doubledNumber}}-->
 <!--  <Quotations></Quotations>-->
 <!--  <Counter></Counter>-->
 <!--  <p>Hello World</p>-->
-  <Slider/>
+<!--  <Slider/>-->
+
+  <input type="text" @input="handleInput">
+
+  {{ greeting }}
 
 <!--  <FirstDemo text="Нажми меня"></FirstDemo>-->
 <!--  <FirstDemo text="Press me"></FirstDemo>-->
@@ -14,6 +20,41 @@ import FirstDemo from "@/components/FirstDemo.vue";
 import Counter from "@/components/Counter.vue";
 import Quotations from "@/components/icons/Quotations.vue";
 import Slider from "@/components/Slider.vue";
+import { computed, ref } from "vue";
+
+const inputValue = ref('')
+
+const handleInput = (e) => {
+  console.log(e)
+  inputValue.value = e.target.value
+  // greetingText = `Hello, ${e.targer.value}`
+}
+
+const greeting = computed({
+  get: () => {
+    if(inputValue.value.length < 5)
+      return `Hello, ${inputValue.value}`
+    else if(inputValue.value.length < 10)
+      return `Hi, ${inputValue.value}`
+    else
+      return `дароу, ${inputValue.value}`
+  },
+  set: (value) => {
+    console.log(value)
+  }
+})
+
+greeting.value = 'b;ab;ab;a'
+
+// const number = ref(0)
+//
+// const doubledNumber = computed(() => {
+//   return number.value * 2
+// })
+//
+// setInterval(() => {
+//   number.value++
+// }, 1000)
 </script>
 
 <style>
--- src/components/Slider.vue
+++ src/components/Slider.vue
@@ -1,8 +1,22 @@
 <template>
-  <img src="" alt="">
+  <img :src="URL[currentLinkIndex]" height="100" alt="">
   <br>
-  <button> < </button>
-  <button> > </button>
+  <button @click="changeSlide(-1)"> < </button>
+  <button @click="changeSlide(1)"> > </button>
 </template>
 <script setup>
+import {ref} from "vue";
+
+const URL = [
+  'https://sponsors.vuejs.org/images/bit.svg?v2',
+  'https://sponsors.vuejs.org/images/appwrite.svg',
+  'https://sponsors.vuejs.org/images/ionic.png?v2',
+]
+
+const currentLinkIndex = ref(0)
+
+const changeSlide = (increment) => {
+  currentLinkIndex.value = (currentLinkIndex.value + increment) % URL.length
+  currentLinkIndex.value = currentLinkIndex.value < 0 ? URL.length - 1 : currentLinkIndex.value
+}
 </script>
