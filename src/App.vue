a9296cb450ad737f4e9996f1fbd24f3df7ff2fbe
Dauren <daukeke99@gmail.com>
2024-03-29 19:45:25 +0500

    scoped slots, watch

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,55 @@
 <template>
-<!--  <SlottedComponent>-->
-<!--    <template #header>-->
-<!--      <p>HEADER</p>-->
+<!--  <Table :items="items">-->
+<!--    <template #price="{ cell }">-->
+<!--      <span :style="`color: ${priceColor(cell)}`">-->
+<!--        {{ cell }}-->
+<!--      </span>-->
 <!--    </template>-->
 
-<!--    <p>ASD</p>-->
-<!--  </SlottedComponent>-->
-  <InputModel v-model:value="value" />
+<!--&lt;!&ndash;    <template #default="{ cell }">&ndash;&gt;-->
+<!--&lt;!&ndash;      <p>default slot - {{ cell }}</p>&ndash;&gt;-->
+<!--&lt;!&ndash;    </template>&ndash;&gt;-->
+<!--  </Table>-->
 
-  {{value}}
+  <input type="color" v-model="color">
 </template>
 
 <script setup>
-import SlottedComponent from "@/components/SlottedComponent.vue";
-import InputModel from "@/components/InputModel.vue";
-import {ref} from "vue";
+import {ref, watch} from "vue";
+import Table from "@/components/Table.vue";
 
-const value = ref()
+const color = ref('#fff')
+
+// 0 - 2000 - green
+// 2001 - 5000 - yellow
+// 5001 - red
+
+const priceColor = (price) => {
+  if(price < 2001) return 'green'
+  else if(price < 5001) return 'yellow'
+
+  return  'red'
+}
+
+const items = [
+  {
+    name: 'Imac',
+    price: 2000,
+    description: 'Desktop'
+  },
+  {
+    name: 'MacBook',
+    price: 3000,
+    description: 'Laptop'
+  },
+  {
+    name: 'Iphone',
+    price: 7000,
+    description: 'Phone'
+  },
+]
+
+watch(color, (newValue) => {
+  document.body.style.background = newValue
+})
 </script>
