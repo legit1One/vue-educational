ec80c8e46af40c09b5591f48a6749b80f6cebe5b
Dauren <daukeke99@gmail.com>
2024-04-06 19:31:01 +0500

    lifecycle hooks

--- src/App.vue
+++ src/App.vue
@@ -1,55 +1,17 @@
 <template>
-<!--  <Table :items="items">-->
-<!--    <template #price="{ cell }">-->
-<!--      <span :style="`color: ${priceColor(cell)}`">-->
-<!--        {{ cell }}-->
-<!--      </span>-->
-<!--    </template>-->
+  <div>
+<!--  <keep-alive>-->
+    <Counter v-if="showCounter" />
+<!--  </keep-alive>-->
 
-<!--&lt;!&ndash;    <template #default="{ cell }">&ndash;&gt;-->
-<!--&lt;!&ndash;      <p>default slot - {{ cell }}</p>&ndash;&gt;-->
-<!--&lt;!&ndash;    </template>&ndash;&gt;-->
-<!--  </Table>-->
-
-  <input type="color" v-model="color">
+  <br><br>
+  <button @click="showCounter = !showCounter">Toggle Counter</button>
+  </div>
 </template>
 
 <script setup>
-import {ref, watch} from "vue";
-import Table from "@/components/Table.vue";
+import Counter from "@/components/Counter.vue";
+import { ref } from "vue";
 
-const color = ref('#fff')
-
-// 0 - 2000 - green
-// 2001 - 5000 - yellow
-// 5001 - red
-
-const priceColor = (price) => {
-  if(price < 2001) return 'green'
-  else if(price < 5001) return 'yellow'
-
-  return  'red'
-}
-
-const items = [
-  {
-    name: 'Imac',
-    price: 2000,
-    description: 'Desktop'
-  },
-  {
-    name: 'MacBook',
-    price: 3000,
-    description: 'Laptop'
-  },
-  {
-    name: 'Iphone',
-    price: 7000,
-    description: 'Phone'
-  },
-]
-
-watch(color, (newValue) => {
-  document.body.style.background = newValue
-})
+const showCounter = ref(true)
 </script>
--- src/components/Counter.vue
+++ src/components/Counter.vue
@@ -1,20 +1,23 @@
 <template>
-  <div>Ваше число: {{ number }}</div>
-<!--  <FirstDemo v-bind:text="number" v-on:click="increment"></FirstDemo>-->
-  <FirstDemo :text="number.toString()" @click="increment"></FirstDemo>
-<!--  <img v-for="(link, index) in URL" :src="link" :key="link" alt="">-->
+  <div>
+    <div id="number">Ваше число: {{ number }}</div>
+  <!--  <FirstDemo v-bind:text="number" v-on:click="increment"></FirstDemo>-->
+    <FirstDemo text="Increment" @click="increment"></FirstDemo>
 
-<!--  <div v-for="(link, idx) in URL" :key="link">-->
-<!--    {{idx}} - {{ link }}-->
-<!--  </div>-->
+  <!--  <img v-for="(link, index) in URL" :src="link" :key="link" alt="">-->
 
-<!--  <div v-for="(value, field) in student" :key="field">-->
-<!--    {{ field }} - {{ value }}-->
-<!--  </div>-->
+  <!--  <div v-for="(link, idx) in URL" :key="link">-->
+  <!--    {{idx}} - {{ link }}-->
+  <!--  </div>-->
+
+  <!--  <div v-for="(value, field) in student" :key="field">-->
+  <!--    {{ field }} - {{ value }}-->
+  <!--  </div>-->
+  </div>
 </template>
 <script setup>
 import FirstDemo from "@/components/FirstDemo.vue";
-import { reactive, ref } from "vue";
+import {onActivated, onBeforeMount, onBeforeUnmount, onDeactivated, onMounted, onUpdated, reactive, ref} from "vue";
 
 const emit = defineEmits(['click-button'])
 
@@ -37,6 +40,50 @@ const increment = () => {
   student.value.age++
   emit('click-button', number.value)
 }
+
+// onBeforeMount
+// onMount
+// onBeforeUpdate => перед тем как че то в темплейте поменялось
+// onUpdate => если че то в темплейте поменялось
+// onBeforeUnmount => перед смертью компонента
+// onActivated => когда компонент появился и он находится внутри keep-alive
+// onDeactivated => когда компонент умер и он находится внутри keep-alive
+
+onBeforeMount(() => {
+  console.log('Before Counter mounted')
+
+  // для запросов и подготовки переменных внутри компонента
+  student.value.name = 'Satoru'
+  // console.log(document.querySelector('#number'))
+})
+
+const callback = () => {
+
+}
+
+onMounted(() => {
+  console.log(document.querySelector('#number'))
+
+  document.addEventListener('click', callback)
+})
+
+onUpdated(() => {
+  console.log("Component updated")
+})
+
+onBeforeUnmount(() => {
+  console.log("Component unmounted")
+
+  document.removeEventListener('click', callback)
+})
+
+onActivated(() => {
+  console.log("Component activated")
+})
+
+onDeactivated(() => {
+  console.log("Component deactivated")
+})
 
 // document.addEventListener('c')
 </script>
