3639ffea0befabff03c7539f15b990b9e2469e30
Dauren <daukeke99@gmail.com>
2024-04-08 19:45:24 +0500

    vue-router

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,51 @@
 <template>
-<!--    <DynamicBg ref="dynamicBg"/>-->
-<!--  <img src="./assets/logo.svg" width="16" alt="">-->
-<!--  <img src="@/assets/logo.svg" width="16" alt="">-->
+  <h1>APP VUE</h1>
+  <!--MPA- multi page application -->
 
-<!--  <br>-->
-<!--  <div class="square" ref="squareEl"></div>-->
+  <a href="/">HOME</a>
+  <br>
+  <a href="/about">ABOUT</a>
+  <br>
+  <a href="/router">ROUTER</a>
+  <br>
+  <br>
+
+  <!-- SPA - single page application -->
+
+  <router-link to="/">HOME</router-link>
+  <br>
+  <router-link to="/about">ABOUT</router-link>
+  <br>
+  <router-link to="/router">ROUTER</router-link>
+  <br>
+  <br>
+
+  <button @click="redirect('/')">Go Home</button>
+  <button @click="redirect('/router')">Go Router</button>
+  <button @click="redirect('/about')">Go About</button>
+  <br>
+  <br>
+
+  <button @click="login">login</button>
+
+  <router-view></router-view>
 </template>
 
 <script setup>
-import {onMounted, ref} from "vue";
-import DynamicBg from "@/components/DynamicBg.vue";
-const squareEl = ref()
-const dynamicBg = ref()
+import { useRouter } from "vue-router";
 
-onMounted(() => {
-  // console.log(document.querySelector('.square'))
-  console.log(squareEl.value)
-  console.log(dynamicBg.value)
-})
+const router = useRouter()
+const redirect = (path) => {
+  console.log(path)
+  router.push(path)
+  // window.location.href = path
+}
+
+const login = () => {
+  // валидация
+  // запрос
+  // обработка
+
+  //редирект на главную/личный кабинет
+}
 </script>
